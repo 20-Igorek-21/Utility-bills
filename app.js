9794d16f47f670d81c4ec21454c684d826e4467a
20-Igorek-21 <igor.kropyvnyi@gmail.com>
2022-04-08 23:02:40 +0300

    project deploy

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 // app.use(compression());
 
-app.use(express.static(path.join(__dirname, 'dist/Utility-bills')));
+app.use(express.static(path.join(__dirname, 'dist/utility-bills')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/Utility-bills/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/utility-bills/index.html'));
 });
 
 const port = process.env.PORT || 3000;
