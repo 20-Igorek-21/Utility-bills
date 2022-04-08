79ef51dc4c1f908ccac79777e73288230f3db30b
20-Igorek-21 <igor.kropyvnyi@gmail.com>
2022-04-09 00:58:33 +0300

    main-layout-page component created

---
+++ src/app/modules/core/layout/layout-page/components/main-layout-page-header/main-layout-page-header.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MainLayoutPageHeaderComponent } from './main-layout-page-header.component';
+
+describe('MainLayoutPageHeaderComponent', () => {
+  let component: MainLayoutPageHeaderComponent;
+  let fixture: ComponentFixture<MainLayoutPageHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MainLayoutPageHeaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MainLayoutPageHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/core/layout/layout-page/components/main-layout-page-header/main-layout-page-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-main-layout-page-header',
+    templateUrl: './main-layout-page-header.component.html',
+    styleUrls: ['./main-layout-page-header.component.css']
+})
+export class MainLayoutPageHeaderComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
