61d85d2a6dcf7e0fa187308d5324ecd994cc9f27
20-Igorek-21 <igor.kropyvnyi@gmail.com>
2022-04-21 20:44:53 +0300

    personal-cabinet-page created

---
+++ src/app/modules/user/personal-cabinet/components/index.ts
@@ -0,0 +1,11 @@
+import {PersonalCabinetFormComponent} from './personal-cabinet-form/personal-cabinet-form.component';
+import {PersonalCabinetFormPageComponent} from './personal-cabinet-form-page/personal-cabinet-form-page.component';
+
+export const personalCabinetComponents = [
+    PersonalCabinetFormComponent,
+    PersonalCabinetFormPageComponent
+]
+
+export * from './personal-cabinet-form/personal-cabinet-form.component';
+export * from './personal-cabinet-form-page/personal-cabinet-form-page.component';
+
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {userRoutes} from './user-routing.module';
+import {PersonalCabinetModule} from "./personal-cabinet/personal-cabinet.module";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        PersonalCabinetModule,
+        RouterModule.forChild(userRoutes)
+    ]
 })
-export class UserModule { }
+export class UserModule {
+}
