75d6ed2cb8a537f9605d330be6314a7fc4e606ec
20-Igorek-21 <igor.kropyvnyi@gmail.com>
2022-05-02 20:47:43 +0300

    user-account service setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'http://node-express-env.eba-dvhxyhrd.eu-central-1.elasticbeanstalk.com/users/',
-    apiUrlAccounts: 'http://node-express-env.eba-dvhxyhrd.eu-central-1.elasticbeanstalk.com/accounts/'
+    apiUrlAccounts: 'http://node-express-env.eba-dvhxyhrd.eu-central-1.elasticbeanstalk.com/accounts/',
 };
